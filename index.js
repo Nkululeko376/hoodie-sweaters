053445114da2c4295d909a124988d07081450c2e
Nkululeko <mtolofakude96@gmail.com>
2025-04-14 17:01:31 +0200

    version2

--- index.js
+++ index.js
@@ -18,21 +18,44 @@ products.forEach( (product) => {
                 </div>
             </div>
     `;
-    console.log(generateHtml);
+    
 });
 document.querySelector('.product-grid').innerHTML = generateHtml;
 
 
-
+//=========== all about cart ============================================
+//================== open cart section ==================================
 let viewCart = document.querySelector('.js-add-to-cart');
-
 viewCart.addEventListener('click', () => {
     document.querySelector('main').classList.add('open-cart-section');
 });
-
+//================= close cart section ======================================
 let hideCart = document.querySelector('.js-remove-cart-section');
 hideCart.addEventListener('click', () => {
     document.querySelector('main').classList.remove('open-cart-section');
 });
+
+//================ remove product from cart ==========================================
+
+if( document.readyState == 'loading' ){
+    document.addEventListener('DOMContentLoaded', ready);
+}
+else {
+    ready();
+}
+
+function ready(){
+    //========to remove product item
+    var removeCartButtons = document.getElementsByClassName('fa-trash');
+    for(var i = 0; i < removeCartButtons.length; i++){
+        var button = removeCartButtons[i];
+        button.addEventListener('click', removeCartItem);
+    }
+}
+
+function removeCartItem(event){
+    var buttonClicked = event.target;
+    buttonClicked.closest('.cart-item').remove();
+}
 
 
